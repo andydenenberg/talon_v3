804af2dff614754ad5a5bb12ec9b6ed8a42c5041
andydenenberg <andydenenberg@gmail.com>
2012-07-07 19:41:38 -0500

    referenced env, not yaml

--- config/environment.rb
+++ config/environment.rb
@@ -5,13 +5,13 @@ require File.expand_path('../application', __FILE__)
 Talon::Application.initialize!
 
 # uncomment the following line to run in development mode
- config = YAML.load(File.read('../../../desktop/config.yml'))   
+# config = YAML.load(File.read('../../../desktop/config.yml'))   
 
 ActionMailer::Base.smtp_settings = {
-  :user_name =>  config['user_name'],
-  :password => config['password'],
-#  :user_name =>  ENV['SENDGRID_USERNAME'] ,
-#  :password => ENV['SENDGRID_PASSWORD'] ,
+#  :user_name =>  config['user_name'],
+#  :password => config['password'],
+  :user_name =>  ENV['SENDGRID_USERNAME'] ,
+  :password => ENV['SENDGRID_PASSWORD'] ,
   :domain => "ospreypointpartners.com",
   :address => "smtp.sendgrid.net",
   :port => 587,
